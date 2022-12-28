e2c1319e38244c84b4366b336f5a1eb9fbadcdf3
Christopher Stewart <chris.stewart132@gmail.com>
2022-12-28 15:25:01 +1300

    1051. Height Checker

---
+++ 1051. Height Checker.py
@@ -0,0 +1,15 @@
+'''
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+'''
+class Solution(object):
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        sorted_heights = sorted(heights)
+        return sum([sorted_heights[i] != heights[i] for i in range(len(heights))])
