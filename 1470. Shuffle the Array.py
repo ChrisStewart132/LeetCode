4c9ccab765f9ca670e7b0b21243da284d5458e59
Christopher Stewart <chris.stewart132@gmail.com>
2023-06-18 11:52:15 +1200

    .\1470. Shuffle the Array.py Easy

---
+++ 1470. Shuffle the Array.py
@@ -0,0 +1,17 @@
+'''
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+'''
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        output = []
+        for i in range(len(nums)//2):
+            output.append(nums[i])
+            output.append(nums[i+len(nums)//2])
+        return output
