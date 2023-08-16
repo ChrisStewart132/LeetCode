7beaef07b179e46502c04dd70ba92933dfadc4c8
Christopher Stewart <chris.stewart132@gmail.com>
2023-08-16 12:09:57 +1200

    .\2657. Find the Prefix Common Array of Two Arrays.py Medium

---
+++ 2657. Find the Prefix Common Array of Two Arrays.py
@@ -0,0 +1,23 @@
+'''
+You are given two 0-indexed integer permutations A and B of length n.
+
+A prefix common array of A and B is an array C such that C[i] is equal to the count of numbers that are present at or before the index i in both A and B.
+
+Return the prefix common array of A and B.
+
+A sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
+'''
+class Solution(object):
+    def findThePrefixCommonArray(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        output = []
+        a,b = set(), set()
+        for i in range(len(A)):
+            a.add(A[i]), b.add(B[i])
+            output.append(len(a.intersection(b)))
+
+        return output
