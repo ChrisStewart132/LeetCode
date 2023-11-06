4bd214f0366b4d9d0f6715eb3c6cd9ae34d27ad4
Christopher Stewart <chris.stewart132@gmail.com>
2023-11-06 13:22:10 +1300

    .\1503. Last Moment Before All Ants Fall Out of a Plank.py Medium

---
+++ 1503. Last Moment Before All Ants Fall Out of a Plank.py
@@ -0,0 +1,19 @@
+'''
+We have a wooden plank of the length n units. Some ants are walking on the plank, each ant moves with a speed of 1 unit per second. Some of the ants move to the left, the other move to the right.
+
+When two ants moving in two different directions meet at some point, they change their directions and continue moving again. Assume changing directions does not take any additional time.
+
+When an ant reaches one end of the plank at a time t, it falls out of the plank immediately.
+
+Given an integer n and two integer arrays left and right, the positions of the ants moving to the left and the right, return the moment when the last ant(s) fall out of the plank.
+'''
+class Solution(object):
+    def getLastMoment(self, n, left, right):
+        """
+        :type n: int
+        :type left: List[int]
+        :type right: List[int]
+        :rtype: int
+        """
+        x = [max(left) if left else 0, n-min(right) if right else 0]
+        return max(x)
