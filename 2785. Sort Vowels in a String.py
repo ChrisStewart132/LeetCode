d1d181efcdc47499d7780bb39c7f5ca0257d2692
Christopher Stewart <chris.stewart132@gmail.com>
2023-11-27 13:20:04 +1300

    .\2785. Sort Vowels in a String.py Medium

---
+++ 2785. Sort Vowels in a String.py
@@ -0,0 +1,26 @@
+'''
+Given a 0-indexed string s, permute s to get a new string t such that:
+
+    All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+    The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+
+Return the resulting string.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+'''
+class Solution(object):
+    def sortVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowel_set = set(['a','e','i','o','u','A','E','I','O','U'])
+        vowels = [c for c in s if c in vowel_set]
+        sorted_vowels = sorted(vowels, key=lambda x: ord(x), reverse=True)
+        output = ""
+        for c in s:
+            if c in vowel_set:
+                output += sorted_vowels.pop()
+            else:
+                output += c
+        return output
