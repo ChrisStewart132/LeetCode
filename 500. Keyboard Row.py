9af2ee0083f3c8597c52466560cfd0924d495244
Christopher Stewart <chris.stewart132@gmail.com>
2024-02-16 11:52:26 +1300

    .\500. Keyboard Row.py Easy

---
+++ 500. Keyboard Row.py
@@ -0,0 +1,26 @@
+'''
+Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+
+In the American keyboard:
+
+    the first row consists of the characters "qwertyuiop",
+    the second row consists of the characters "asdfghjkl", and
+    the third row consists of the characters "zxcvbnm".
+
+'''
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+        output = []
+        for word in words:
+            s = set(word.lower())
+            if s.issubset(row1) or s.issubset(row2) or s.issubset(row3):
+                output.append(word)
+        return output
+        
