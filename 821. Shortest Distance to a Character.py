18f6ba93b0111584714c69283aa922d026fc3464
Christopher Stewart <chris.stewart132@gmail.com>
2024-02-24 13:01:59 +1300

    .\821. Shortest Distance to a Character.py Easy

---
+++ 821. Shortest Distance to a Character.py
@@ -0,0 +1,14 @@
+"""
+Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length and answer[i] is the distance from index i to the closest occurrence of character c in s.
+
+The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+"""
+class Solution(object):
+    def shortestToChar(self, s, c):
+        """
+        :type s: str
+        :type c: str
+        :rtype: List[int]
+        """
+        positions = [i for i in range(len(s)) if s[i] == c]
+        return [min([abs(pos-i) for pos in positions]) for i in range(len(s))]
