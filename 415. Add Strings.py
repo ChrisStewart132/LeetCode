ee0a6c82ef96ddfe1fc5659e9ef9328bff8f5c41
Christopher Stewart <chris.stewart132@gmail.com>
2024-02-29 12:43:09 +1300

    .\415. Add Strings.py Easy

---
+++ 415. Add Strings.py
@@ -0,0 +1,13 @@
+'''
+Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+'''
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1) + int(num2))
