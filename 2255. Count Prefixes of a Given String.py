93ef63366c46afaa0f984790ea2e2376a887055c
Christopher Stewart <chris.stewart132@gmail.com>
2024-03-19 08:47:35 +1300

    .\2255. Count Prefixes of a Given String.py Easy

---
+++ 2255. Count Prefixes of a Given String.py
@@ -0,0 +1,15 @@
+'''
+You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+
+Return the number of strings in words that are a prefix of s.
+
+A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+'''
+class Solution(object):
+    def countPrefixes(self, words, s):
+        """
+        :type words: List[str]
+        :type s: str
+        :rtype: int
+        """
+        return sum([len(word) <= len(s) and s.find(word) == 0 for word in words])
