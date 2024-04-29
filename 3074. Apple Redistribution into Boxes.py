1296a088e9c93de335109a0165c75d09146928c0
Christopher Stewart <chris.stewart132@gmail.com>
2024-04-30 08:44:28 +1200

    .\3074. Apple Redistribution into Boxes.py Easy

---
+++ 3074. Apple Redistribution into Boxes.py
@@ -0,0 +1,23 @@
+'''
+You are given an array apple of size n and an array capacity of size m.
+
+There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+
+Note that, apples from the same pack can be distributed into different boxes.
+'''
+class Solution(object):
+    def minimumBoxes(self, apple, capacity):
+        """
+        :type apple: List[int]
+        :type capacity: List[int]
+        :rtype: int
+        """
+        c = sorted(capacity)
+        n_apples = sum(apple)
+        output = 0
+        while n_apples > 0:
+            n_apples -= c.pop()
+            output += 1
+        return output
